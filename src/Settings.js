a48bdae8c1e413427b50eb1c99f47d3646ecbaef
Magdalena Glas <56884203+lenaglas@users.noreply.github.com>
2021-10-28 10:13:07 +0200

    Update Settings.js

--- src/Settings.js
+++ src/Settings.js
@@ -1,10 +1,8 @@
-import { faCloudShowersHeavy } from "@fortawesome/free-solid-svg-icons";
-
 //Change settings to turnOff the Kibana dashboard and disable user login for testing
 const settings = {
 
-    kibanaOn: false,
-    loginDisabled: faCloudShowersHeavy,
+    kibanaOn: true,
+    loginDisabled: true,
 
 }
 
